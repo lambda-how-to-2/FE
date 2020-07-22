e09cc2f222d29e83aa2d0eb3d5bfffe974103aaa
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-07-22 09:46:06 -0700

    Two new folders, reducers and actions, each with an index file. Actions is empty for now, but the reducer has an initialState of just a howto array, and a reducer that is just returning state at this time.

---
+++ howtoapp/src/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+    howToDos: []
+}
+
+export const HowToReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
