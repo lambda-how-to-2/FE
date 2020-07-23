0ff4dafa5448cb480cf7a741b69d09ce0be6ec5f
noah niesen <noah@noahs-MacBook-Pro.local>
2020-07-22 22:33:20 -0500

    installed required packages, wrapped App in a router (index.js), created a components folder that includes Input.js, Login.js, Register.css, and Register.js. Styled Register.js, and form functionality is working properly.

---
+++ howtoapp/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div>
+      <div className='welcome-back'>
+        <h1>Welcome Back</h1>
+      </div>
+    </div>
+  );
+}
---
+++ howtoapp/src/components/Register.js
@@ -0,0 +1,132 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Input from './Input';
+import * as yup from 'yup';
+import axios from 'axios';
+import './Register.css';
+
+export default function Register() {
+  const defaultState = {
+    "Full Name": "",
+    "Email": "",
+    "Username": "",
+    "Password": ""
+  };
+
+  const [formState, setFormState] = useState(defaultState);
+  const [errors, setErrors] = useState(defaultState);
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  // formState schema
+  const formSchema = yup.object().shape({
+    "Full Name": yup.string().required('Please enter your name.'),
+    "Email": yup
+      .string()
+      .required("Please enter your email.")
+      .email("This is not a valid email."),
+    "Username": yup
+      .string()
+      .required("Please enter a username.")
+      .min(3, "Username must be at least 3 characters long."),
+    "Password": yup
+      .string()
+      .required("Please enter a password.")
+      .min(8, "Password must be at least 8 characters long.")
+  });
+
+  useEffect(() => {
+    formSchema.isValid(formState).then(valid => setButtonDisabled(!valid));
+  }, [formState]);
+
+  // onSubmit function
+  const formSubmit = e => {
+    e.preventDefault();
+    console.log("form submitted!");
+    axios
+      .post("https://reqres.in/api/users", formState)
+      .then(() => console.log("form submitted success!"))
+      .catch((err) => console.log(err));
+  };
+
+  // validate whether value meets schema
+  const validateChange = e => {
+    e.persist();
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .then(valid =>
+        setErrors({
+          ...errors,
+          [e.target.name]: ""
+        })
+      )
+      .catch(error =>
+        setErrors({
+          ...errors,
+          [e.target.name]: error.errors[0]
+        })
+      );
+  };
+
+  // onChange function
+  const inputChange = e => {
+    const value = e.target.value;
+    setFormState({...formState, [e.target.name]: value});
+    validateChange(e);
+  };
+
+  const errorMessages = e => {
+    // const errorMessage = props.errors[props.name];
+    // {errorMessage.length !== 0 && <p className="error">{errorMessage}</p>}
+  };
+
+  return (
+    <form onSubmit={formSubmit}>
+      <div className="welcome">
+        <h1>Welcome</h1>
+      </div>
+      <div className="inputs">
+        <Input
+          type="text"
+          name="Full Name"
+          onChange={inputChange}
+          value={formState["Full Name"]}
+          placeholder="Full Name"
+        />
+        <Input
+          type="text"
+          name="Email"
+          onChange={inputChange}
+          value={formState["Email"]}
+          placeholder="Email"
+        />
+        <Input
+          type="text"
+          name="Username"
+          onChange={inputChange}
+          value={formState["Username"]}
+          placeholder="Username"
+        />
+        <Input
+          type="password"
+          name="Password"
+          onChange={inputChange}
+          value={formState["Password"]}
+          placeholder="Password"
+        />
+      </div>
+      <div className="already-member">
+        <p>Already a member?</p>
+        <Link to="/login">Log In</Link>
+      </div>
+      <button disabled={buttonDisabled}>Sign up</button>
+      <div className='errors'>
+        {Object.keys(errors).map((key, i) => {
+          if (errors[key].length > 0) {
+            return <p key={i}>{errors[key]}</p>
+          }
+        })}
+      </div>
+    </form>
+  );
+}
