7fbab867f2c4110e6e80955c8d80c159a39b1c4d
Bruno Paula <brunopaula@dryserv.com>
2020-07-23 02:09:41 -0400

    Created Logged in state Navigation

--- howtoapp/src/components/navigation/LoggedInNav.jsx
+++ howtoapp/src/components/navigation/LoggedInNav.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+
 import './navigation.style.css'
 
 function LoggedInNav() {
     return (
-        <div>
-
-        </div>
+        <>
+            <div className="nav-wrapper">
+                <nav>
+                    <h1 className="nav-logo-loggedIn">HOW2</h1>
+                    <div className="searchBox">
+                        <input type="text" id="search" placeholder="Seach" />
+                        <button className='searchBtn'><i class="fa fa-search"></i></button>
+                    </div>
+                    <div className="menu">
+                        <a href="/explorer">Explore</a>
+                        <a href="/explorer">Profile</a>
+                        <a href="/explorer">Logout</a>
+                    </div>
+                </nav>
+            </div>
+            {/* <hr className="hr-loggedIn" /> */}
+        </>
     )
 }
 
