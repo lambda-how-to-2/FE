70a865aad7de3686c4aaa8d6099a5d6350edf39a
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-07-23 17:16:36 -0700

    Just added redux to the index file

--- howtoapp/src/reducers/index.js
+++ howtoapp/src/reducers/index.js
@@ -6,6 +6,21 @@ const initialState = {
 
 export const HowToReducer = (state = initialState, action) => {
     switch (action.type) {
+        case "EDIT_CARD":
+            return {
+                ...state,
+                isEditing: true,
+                cardToEdit: action.payload
+            }
+        case "RETURN_CARD":
+            return {
+                ...state,
+                isEditing: false,
+                list: [
+                    ...state.list,
+                    
+                ]
+            }
         default:
             return state;
     }
