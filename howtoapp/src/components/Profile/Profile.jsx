b94462754a0d254e887987638e3272d0e8cbfe67
Bruno Paula <brunopaula@dryserv.com>
2020-07-23 23:22:31 -0400

    Styling Bug fixes

--- howtoapp/src/components/Profile/Profile.jsx
+++ howtoapp/src/components/Profile/Profile.jsx
@@ -1,33 +1,28 @@
 import React from 'react'
 import './profile_style.css'
-import Navigation from '../navigation/LoggedInNav'
 import ImageThumbnail from './image-thumbnail.svg'
 
 
 
-function Profile({ searchBox }) {
+function Profile() {
     return (
         <div className='profile'>
-            <Navigation searchBox={searchBox} />
-            <div className="conatianer">
-                <div className="wrapper">
-                    <form>
-                        <input className="profile-title " name="title" id="profileTitle" type="text" placeholder="Title" />
-                        <textarea className="profile-body" rows="10" cols="30" placeholder="Body" />
-                        <div className="formBottomBtn">
-                            <div className="upload-btn">
-                                <label for="file-upload" class="file-upload-circle">
-                                    <img src={ImageThumbnail} alt="" srcset="" />
-                                </label>
-                                <input id="file-upload" type="file" />
-                            </div>
-                            {/* <p className="form-error error">if an error </p> */}
-                            <button className="post-btn">Post</button>
+            <form>
+                <div className="profile-form">
+                    <input className="profile-title" name="title" id="profileTitle" type="text" placeholder="Title" />
+                    <textarea className="profile-body" rows="10" cols="30" placeholder="Body" />
+                    <div className="formBottomBtn">
+                        <div className="upload-btn">
+                            <label htmlFor="file-upload" className="file-upload-circle">
+                                <img src={ImageThumbnail} alt="upload icon" />
+                            </label>
+                            <input id="file-upload" type="file" />
+                            <button className="profile-post-btn">Post</button>
                         </div>
-                        {/* <p className="form-error error">if an error </p> */}
-                    </form>
+                    </div>
+                    <p className="profile-error">if an error if an errorif an error </p>
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
