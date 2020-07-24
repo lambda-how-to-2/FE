86e0c886882adf618597a040f6979ec202149fd4
Bruno Paula <brunopaula@dryserv.com>
2020-07-23 23:20:30 -0400

    Added some routes and navs

--- howtoapp/src/App.js
+++ howtoapp/src/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+
 import Register from './components/Register';
 import Login from './components/Login';
+import Profile from './components/Profile/Profile'
+import Home from './components/Home/home'
+import LoggedInNav from './components/navigation/LoggedInNav'
+import Navigation from './components/navigation/Navigation';
+import Error404 from './components/Error404'
+
 
 function App() {
-  const [searchBox, setSearchBox] = React.useState(true)
+  const [loggedIn, setLoggedIn] = React.useState(true)
+
+
+  const isLoggedIn = loggedIn ? <Navigation /> : <LoggedInNav />
 
   return (
-    <div className="App">
+    <>
+      {isLoggedIn}
       <Switch>
-        <Route path='/register' component={Register}/>
-        <Route path='/login' component={Login}/>
+        <Route path='/profile' component={Profile} />
+        <Route path='/register' component={Register} />
+        <Route path='/login' component={Login} />
+        <Route path='/' exact component={Home} />
+        <Route path='*' exact component={Error404} />
       </Switch>
-    </div>
+    </>
 
   );
 }
