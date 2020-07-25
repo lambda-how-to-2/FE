9440a6959e3b0b3e6955b9ab718f71594113c829
Bruno Paula <brunopaula@dryserv.com>
2020-07-24 22:28:51 -0400

    Add React Router Link to the buttons

--- howtoapp/src/components/Home/home.jsx
+++ howtoapp/src/components/Home/home.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import './home_style.css'
+import { Link } from 'react-router-dom'
 
 
 function home() {
   return (
     <div className="home">
-      {/* Temp Nav untill we add react router  */}
       <div className="btn-container">
         <div className="wrapper">
           <h1 className="home-title">What would you like to do?</h1>
           <div className="btn_wrapper">
             <div className="discover_btn">
-              <div className="icon">
-                <h2>Discover</h2>
+              <div className="icon-discover">
+                <Link to='/howtos'><h2>Discover</h2></Link>
+
               </div>
             </div>
             <div className="create_btn">
-              <div className="icon2">
-                <h2>Create</h2>
+              <div className="icon-create">
+                <Link to="/profile"><h2>Create</h2></Link>
               </div>
             </div>
           </div>
